b4b7d75f0a6b8cd516af5cb6214f5808ac81a5b2
freetimezp <kraftukrnet@ukr.net>
2022-08-22 21:03:16 +0300

    03.Js-chrome-extension. No youtube in url and no bookmarks message in popup.

--- public/popup.js
+++ public/popup.js
@@ -1,7 +1,21 @@
+import {getActiveTabURL} from "./utils.js";
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
-const viewBookmarks = () => {};
+const viewBookmarks = (currentBookmarks = []) => {
+    const bookmarksElement = document.getElementById("bookmarks");
+    bookmarksElement.innerHTML = "";
+
+    if(currentBookmarks.length > 0) {
+        for(let i = 0; i < currentBookmarks.length; i++) {
+            const bookmark = currentBookmarks[i];
+            addNewBookmark(bookmarksElement, bookmark);
+        }
+    }else{
+        bookmarksElement.innerHTML = '<i class="row">No bookmarks to show.</i>';
+    }
+};
 
 const onPlay = e => {};
 
@@ -9,4 +23,21 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            //view bookmarks
+            viewBookmarks(currentVideoBookmarks);
+        })
+    }else{
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class="title">This is not youtube video page.</div>';
+    }
+});
