9ca994b56caaf770d144a69d46d8367e34840220
freetimezp <kraftukrnet@ukr.net>
2022-08-23 19:47:23 +0300

    04.Js-chrome-extension. Add play btn in bookmark and play video from this time.

--- public/popup.js
+++ public/popup.js
@@ -4,6 +4,7 @@ import {getActiveTabURL} from "./utils.js";
 const addNewBookmark = (bookmarksElement, bookmark) => {
     const bookmarkTitleElement = document.createElement("div");
     const newBookmarkElement = document.createElement("div");
+    const controlsElement = document.createElement("div");
 
     bookmarkTitleElement.innerText = bookmark.desc;
     bookmarkTitleElement.className = "bookmark-title";
@@ -12,7 +13,12 @@ const addNewBookmark = (bookmarksElement, bookmark) => {
     newBookmarkElement.className = "bookmark";
     newBookmarkElement.setAttribute("timestamp", bookmark.time);
 
+    controlsElement.className = "bookmark-controls";
+
+    setBookmarkAttributes("play", onPlay, controlsElement);
+
     newBookmarkElement.appendChild(bookmarkTitleElement);
+    newBookmarkElement.appendChild(controlsElement);
     bookmarksElement.appendChild(newBookmarkElement);
 };
 
@@ -30,11 +36,26 @@ const viewBookmarks = (currentBookmarks = []) => {
     }
 };
 
-const onPlay = e => {};
+const onPlay = async e => {
+    const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
+    const activeTab = await getActiveTabURL();
+
+    chrome.tabs.sendMessage(activeTab.id, {
+        type: "PLAY",
+        value: bookmarkTime
+    });
+};
 
 const onDelete = e => {};
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes =  (src, eventListener, controlParentElement) => {
+    const controlElement = document.createElement("img");
+    controlElement.src = "assets/" + src + ".png";
+    controlElement.title = src;
+    controlElement.addEventListener("click", eventListener);
+
+    controlParentElement.appendChild(controlElement);
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
     const activeTab = await getActiveTabURL();
